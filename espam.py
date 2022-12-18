9dddaec84a22cfef6c6cc7896765295dd54c9308
Jake Bryant <65371714+c0urted@users.noreply.github.com>
2022-12-17 23:55:37 -0500

    works for sms now

--- espam.py
+++ espam.py
@@ -2,16 +2,58 @@ import smtplib
 import time
 import random
 import os
+import fade
 import colorama
 from colorama import Fore, Back, Style
 from random_word import RandomWords
 
+fade_me = """
+8888888888 .d8888b.                                  
+888       d88P  Y88b                                 
+888       Y88b.                                      
+8888888    "Y888b.   88888b.   8888b.  88888b.d88b.  
+888           "Y88b. 888 "88b     "88b 888 "888 "88b 
+888             "888 888  888 .d888888 888  888  888 
+888       Y88b  d88P 888 d88P 888  888 888  888  888 
+8888888888 "Y8888P"  88888P"  "Y888888 888  888  888 
+                     888                             
+                     888                             
+                     888                      By: c0urted"""
+faded = fade.purplepink(fade_me)
+print(faded)
+
 colorama.init()
-print(Fore.LIGHTCYAN_EX + "")
+print(Fore.MAGENTA + "")
 
 user = "EMAIL HERE"
 passwd = "PASSWORD HERE"
-victim = input("please input victim email here\n")
+
+print("sms format is like 1234567890@txt.att.net")
+victim = input("please input victim email or phone here\n")
+isnum = input("is this a number [Y/N]")
+
+def sms_sender():
+    r = RandomWords()
+    x = r.get_random_word()
+    y = r.get_random_word()
+    z = r.get_random_word()
+    msg = x + " " + y + " " + z
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(user , passwd)
+    server.sendmail(
+        user,
+        victim,
+        msg)
+    server.quit()
+
+def sms_spam():
+    spam_count = int(input("how many texts to send?"))
+    for i in range(spam_count):
+            sms_sender()
+            print("sms message sent!")
+            zzz = random.randint(1,9)
+            time.sleep(zzz)
+
 
 def mail_sender():
     r = RandomWords()
@@ -22,7 +64,6 @@ def mail_sender():
     subject = w
     msg = x + " " + y + " " + z
     message = "Subject:" + subject + "\n" + msg
-#You can edit the line below to change to a different smtp service  
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login(user , passwd)
     server.sendmail(
@@ -38,5 +79,9 @@ def spam():
             print("message sent!")
             zzz = random.randint(1,9)
             time.sleep(zzz)
-
 spam()
+
+if isnum.lower().startswith("y") == True:
+    sms_spam()
+else:
+    spam()
