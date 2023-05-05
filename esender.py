62f15369944cfc1431d1eaf9b9cde2de413f81c5
courted <65371714+c0urted@users.noreply.github.com>
2023-05-04 20:13:44 -0400

    right file this time

--- esender.py
+++ esender.py
@@ -13,6 +13,8 @@ from email.mime.text import MIMEText
 # Todo:
 #  add unicode invis symbols to get around spam filters
 #  fix user input so it only takes numbers
+#  check for bounced emails
+#  create a count for bounced emails and show it on the menu
 
 fade_me = """
 8888888888 .d8888b.                                  
@@ -30,46 +32,111 @@ fade_me = """
 
 xyz = random.randint(0,1000000)
 idnum = xyz
-try:
-    spamlist = open("spamlist.txt", "r")
-except FileNotFoundError:
-    print("file not found")
-    spamlist = open("spamlist.txt", "w+")
-    print("empty spamlist.txt created. Please enter your targs there and restart esender")
-    time.sleep(5)
 
+def file_check():
+    try:
+        # spamlist might not work. dont wanna fix rn do later.
+        global spamlist
+        spamlist = open("spamlist.txt", "r")
+    except FileNotFoundError:
+        print("Spamlist.txt not found. Creating it now.")
+        with open("spamlist.txt", "w+") as spamlist:
+            print("Spamlist.txt has been created. Please enter your targs there and restart the sender.")
+            time.sleep(5)
+
+    try:
+        with open("login.txt", "r") as login:
+            login_info = login.readline().strip().split(":")
+            email = login_info[0]
+            passwd = login_info[1]
+            smtp_address = login_info[2]
+            smtp_port = int(login_info[3])
+            encryption = int(login_info[4])
+
+    except FileNotFoundError:
+        print("login.txt not found. Making it now.")
+        enter_login()
+
+    # except IndexError:
+    #     print("Login file missing login info. Please enter it now.")
+    #     enter_login()
+    return email, passwd, smtp_address, smtp_port, encryption
+
+def enter_login():
+    with open("login.txt", "w+") as login:
+        email = input("Enter your email address: ")
+        passwd = input("Enter your password: ")
+        smtp_address = input("Enter your SMTP server: ")
+        smtp_port = int(input("Enter your smtp port: "))
+        encryption = input("Does your smtp use any ecnryption?\n1) SSL\n2) TLS\n3) None\n")
+        login.write(f"{email}:{passwd}:{smtp_address}:{smtp_port}:{encryption}")
 
 def main():
+    email, passwd, smtp_address, smtp_port, encryption = file_check()
     clear_terminal()
-    menu_options = int(input("Welcome to Espam!\nUse the number keys to navigate the menu\nWhat would you like to do?\n1) SMS Sendouts\n2) Email Sendouts\n3) Exit\n"))
+    while True:
+        try:
+            menu_options = int(input("Welcome to Espam!\nUse the number keys to navigate the menu\nWhat would you like to do?\n1) SMS Sendouts\n2) Email Sendouts\n3) Edit login info\n0) Exit\n"))
+            break
+        except ValueError:
+            clear_terminal()
+            print("Please use 0-9 for the menu.")
+        except KeyboardInterrupt:
+            clear_terminal()
+            print("\nProgram terminated by user.")
+
     clear_terminal()
-    while menu_options > 3 or menu_options <=0:
-        menu_options = int(input("How could you fuck this up?? Just press a fucking number!!\n1) SMS Sendouts\n2) Email Sendouts\n3) Exit\n"))
-# login for smtp server here
-    email = "Put SMTP email here"
-    passwd = "Put SMTP password here"
-    smtp_address = "smtp.gmail.com"
-    smtp_port = 465
-    clear_terminal()
-    if menu_options == 1:
-        sms(email, passwd, smtp_address, smtp_port)
-    elif menu_options == 2:
-        clear_terminal()
-        letter_choice = int(input("Do you have a letter to use?\n1) Yes\n2) No\n"))
-        while letter_choice < 1 or letter_choice > 2:
-            print("Fix your input retard. 1 or 2 its not that fucking hard\n")
-        if letter_choice == 1:
-            email_letter_sendout(email, passwd, smtp_address, smtp_port)
+    try:
+        if menu_options == 1:
+            if encryption == 1:
+                sms_ssl(email, passwd, smtp_address, smtp_port)
+            elif encryption == 2:
+                sms_tls(email, passwd, smtp_address, smtp_port)
+            else:
+                sms_no_encryption(email, passwd, smtp_address, smtp_port)
+
+        elif menu_options == 2:
+            while True:
+                clear_terminal()
+                try:
+                    letter_choice = int(input("Do you have a letter to use?\n1) Yes\n2) No\n0) Exit\n"))
+                    if letter_choice == 1:
+                        email_letter_sendout(email, passwd, smtp_address, smtp_port)
+                        break
+                    elif letter_choice == 2:
+                        email_sendout(email, passwd, smtp_address, smtp_port)
+                        break
+                    elif letter_choice == 0:
+                        exit()
+                    else:
+                        print("Please pick 1, 2 or 0")
+                        time.sleep(3)
+                except ValueError:
+                    print("Please enter a number.")
+                    time.sleep(3)
+
+        elif menu_options == 3:
+            with open("login.txt", "w+") as login:
+                email = input("Enter your email address: ")
+                passwd = input("Enter your password: ")
+                smtp_address = input("Enter your SMTP server: ")
+                smtp_port = int(input("Enter your smtp port: "))
+                encryption = input("Does your smtp use any ecnryption?\n1) SSL\n2) TLS\n3) None\n")
+                login.write(f"{email}:{passwd}:{smtp_address}:{smtp_port}:{encryption}")
+                print("SMTP login info updated!")
+                time.sleep(2)
+                main()
         else:
-            email_sendout(email, passwd, smtp_address, smtp_port)
-    elif menu_options == 3:
-        #################
-        # add email validator
-        # 
-        #################
-        pass
-    else:
-        exit
+            exit
+    except ValueError:
+        print("stuff")
+    except smtplib.SMTPAuthenticationError:
+        print("Error: Incorrect email or password.")
+    except smtplib.SMTPConnectError:
+        print("Error: Could not connect to SMTP server.")
+    except smtplib.SMTPException as fuckup:
+        print("Error:", fuckup)
+
 
 # clears terminal and prints menu logo
 def clear_terminal():
@@ -91,7 +158,8 @@ def email_letter_sendout(user, passwd, smtp_address, smtp_port):
     # replace it with smtplib.SMTP instead
     server = smtplib.SMTP_SSL(smtp_address, smtp_port)
     server.login(user, passwd)
-    html = open("paypal.html")
+    html_letter_name = input("Please make sure your letter is in the same folder as the sender.\nPlease input the filename of the letter: ")
+    html = open(html_letter_name)
     msg = MIMEText(html.read(), "html")
     msg["subject"] = f"Account Case [{idnum}]"
     for i in spamlist:
@@ -101,7 +169,8 @@ def email_letter_sendout(user, passwd, smtp_address, smtp_port):
             i,
             msg.as_string())
         server.quit()
-        print("message sent to", i)
+        print("[*]\tMessage sent to:", i)
+# Change the numbers for seconds to sleep between emails sent
         x = random.randint(8,20)
         time.sleep(x)
 
@@ -123,7 +192,22 @@ def email_sendout(user, passwd, smtp_address, smtp_port):
         time.sleep(x)
 
 
-def sms(user, passwd, smtp_address, smtp_port):
+def sms_no_encryption(user, passwd, smtp_address, smtp_port):
+    server = smtplib.SMTP(smtp_address, smtp_port)
+    server.login(user, passwd)
+    date_alert = datetime.datetime.now() + datetime.timedelta(days=30)
+    message = f"PayPal Account Service DPT\nCase[{idnum}]\nYour account has been locked due to fraudulent activity. Please contact us by:{date_alert:%m-%d-%Y} or the account will be permanently disabled."
+    for i in spamlist:
+        server.sendmail(
+            user,
+            i,
+            message)
+        server.quit()
+        print("message sent to", i)
+        time.sleep(1)
+
+
+def sms_ssl(user, passwd, smtp_address, smtp_port):
     server = smtplib.SMTP_SSL(smtp_address, smtp_port)
     server.login(user, passwd)
     date_alert = datetime.datetime.now() + datetime.timedelta(days=30)
@@ -138,4 +222,21 @@ def sms(user, passwd, smtp_address, smtp_port):
         time.sleep(1)
 
 
-main()
+def sms_tls(user, passwd, smtp_address, smtp_port):
+    server = smtplib.SMTP(smtp_address, smtp_port)
+    server.starttls()
+    server.login(user, passwd)
+    date_alert = datetime.datetime.now() + datetime.timedelta(days=30)
+    message = f"PayPal Account Service DPT\nCase[{idnum}]\nYour account has been locked due to fraudulent activity. Please contact us by:{date_alert:%m-%d-%Y} or the account will be permanently disabled."
+    for i in spamlist:
+        server.sendmail(
+            user,
+            i,
+            message)
+        server.quit()
+        print("message sent to", i)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
